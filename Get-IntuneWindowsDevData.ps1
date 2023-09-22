69844cc1a3b0beb9952aaa8b06cbe7acf08464b9
VivekPandey3692 <138861331+VivekPandey3692@users.noreply.github.com>
2023-09-22 19:11:33 +0530

    Script to fetch Intune Windows devices inventory

---
+++ Get-IntuneWindowsDevData.ps1
@@ -0,0 +1,6 @@
+﻿#Connect-MSGraph
+Start-Transcript C:\Temp\IntuneManagedDevices.txt
+$IntuneDevices = Get-IntuneManagedDevice -filter "operatingSystem eq 'Windows'" | Get-MSGraphAllPages
+$IntuneManagedDevices = $IntuneDevices | select deviceName,complianceState,lastSyncDateTime,osVersion | FT -Wrap
+$IntuneManagedDevices
+Stop-Transcript
