c040854c35b87d29455765a0e6afeb487e43471c
VivekPandey3692 <138861331+VivekPandey3692@users.noreply.github.com>
2023-09-22 19:20:19 +0530

    Update Get-IntuneWindowsDevData.ps1

--- Get-IntuneWindowsDevData.ps1
+++ Get-IntuneWindowsDevData.ps1
@@ -1,4 +1,14 @@
-﻿#Connect-MSGraph
+<#
+Title: Graph to Fetch Windows device inventory from Intune
+Description: The Powershell script uses MS Graph to fetch the Windows devices data from Intune.
+Author: Vivek kumar Pandey
+Version: 1.0
+#>
+
+#Execute Connect-msgraph command once for authentication and then comment it placing '#' as pre-fix.
+Connect-MSGraph
+
+#Generate execution log using transcript
 Start-Transcript C:\Temp\IntuneManagedDevices.txt
 $IntuneDevices = Get-IntuneManagedDevice -filter "operatingSystem eq 'Windows'" | Get-MSGraphAllPages
 $IntuneManagedDevices = $IntuneDevices | select deviceName,complianceState,lastSyncDateTime,osVersion | FT -Wrap
